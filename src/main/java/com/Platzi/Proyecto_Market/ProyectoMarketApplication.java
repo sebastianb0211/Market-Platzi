7938fa13efede9966f740442c876e0aaf6ede095
Sebastianb <sebastianb.0211@gmail.com>
2024-12-16 14:41:23 -0500

    se crea nuevo proyecto

---
+++ src/main/java/com/Platzi/Proyecto_Market/ProyectoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.Platzi.Proyecto_Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Platzi/Proyecto_Market/ProyectoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Platzi.Proyecto_Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
